a745af012515c2b862603914d813dfaa0a4276ee
Aran <aransomnee@hotmail.com>
2016-07-09 14:13:44 +0700

    SignUp Successful

---
+++ app/src/main/java/aran/appsomnee/aranebookshop/MyAlert.java
@@ -0,0 +1,31 @@
+package aran.appsomnee.aranebookshop;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by aransomnee on 7/9/16 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context); // Create Constance or object
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.nobita48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+} //Main Class
+    //Class obejext = new class();
+
