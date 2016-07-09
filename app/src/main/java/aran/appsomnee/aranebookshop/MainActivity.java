0301c217de1b8bcf8f419127fa3499cee2361a75
Aran <aransomnee@hotmail.com>
2016-07-09 09:48:45 +0700

    Start App

---
+++ app/src/main/java/aran/appsomnee/aranebookshop/MainActivity.java
@@ -0,0 +1,13 @@
+package aran.appsomnee.aranebookshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
